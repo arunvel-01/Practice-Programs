467d30241ed200312502a3e6ee4c23965c4e1fc0
arunvel-01 <133992095+arunvel-01@users.noreply.github.com>
2023-12-13 21:41:48 +0530

    Create Pracrice.c

---
+++ Pracrice.c
@@ -0,0 +1,14 @@
+Odd or Even:
+
+# include<stdio.h>
+int main()
+{
+    int num;
+    printf("Enter the number: ");
+    scanf("%d",&num);
+    if(num%2==0){
+        printf("Even");
+    } else{
+        printf("Odd");
+    }
+}
