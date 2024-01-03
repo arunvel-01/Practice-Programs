05285ce02ec5fa86084ee1e9b5f5a11bcea76166
arunvel-01 <133992095+arunvel-01@users.noreply.github.com>
2024-01-03 20:16:22 +0530

    Update Practice.c

--- Practice.c
+++ Practice.c
@@ -12,3 +12,18 @@ int main()
         printf("Odd");
     }
 }
+
+Last digit in a number:
+
+#include <stdio.h>
+
+int lastDigit(int n){
+    return (n % 10);
+}
+
+int main(){
+    int input;
+    printf("Enter the number: ");
+    scanf("%d", &input);
+    printf("%d\n", lastDigit(input));
+}
