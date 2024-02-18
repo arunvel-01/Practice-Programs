0b69b3bced9921606d453646c3db20c7355c6e31
arunvel-01 <133992095+arunvel-01@users.noreply.github.com>
2024-02-18 22:20:39 +0530

    Update Practice.c

--- Practice.c
+++ Practice.c
@@ -135,3 +135,30 @@ int main(){
     printf("\n");
     
 }
+
+Fibonacci Series Generator:
+
+#include<stdio.h>
+
+void fibonacci(int n){
+    int first=0,second=1,next;
+    printf("Fibonacci Series up to %d terms:\n", n);
+    for(int i=0;i<=n;i++){
+        if(i <= 1) {
+        next=i;
+    } else {
+        next=first+second;
+        first=second;
+        second=next;
+    }
+    printf("%d ",next);
+    }
+    printf("\n");
+}
+
+int main(){
+    int n;
+    printf("Enter the number of  terms: ");
+    scanf("%d",&n);
+    fibonacci(n);
+}
